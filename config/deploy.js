5d7fce1ce7ff6080ba8d86497696bf2c32e77b18
Marcos <devmarcosweb@gmail.com>
2018-05-07 02:02:53 -0300

    s3 keys update

--- config/deploy.js
+++ config/deploy.js
@@ -5,21 +5,31 @@ module.exports = function(deployTarget) {
   let ENV = {
     build: {},
   };
-  
-  const s3 = {
-    accessKeyId: 'AKIAIMFMQFJXO5WVTWTA',
-    secretAccessKey: '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',
-    bucket: 'ember-pokemon',
-    region: 'us-east-1'
-  };
-  
+
+  // const s3 = {
+  //   accessKeyId: 'AKIAI3EMHYY5T7WGOAFQ',
+  //   secretAccessKey: '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',
+  //   bucket: 'ember-pokemon',
+  //   region: 'us-east-1'
+  // };
+
   if (deployTarget === 'development') {
     ENV.build.environment = 'development';
-  } 
+  }
   else if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    ENV.s3 = s3,
-    ENV['s3-index'] = s3
+    ENV.s3 = {
+      accessKeyId: 'AKIAI3EMHYY5T7WGOAFQ',
+      secretAccessKey: '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',
+      bucket: 'ember-pokemon',
+      region: 'us-east-1'
+    },
+    ENV['s3-index'] = {
+      accessKeyId: 'AKIAI3EMHYY5T7WGOAFQ',
+      secretAccessKey: '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',
+      bucket: 'ember-pokemon',
+      region: 'us-east-1'
+    }
   }
 
   return ENV;
